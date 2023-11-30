19f556086229ac0af3fd2055d154c49239f60f76
David <Rigby335@hotmail.com>
2023-11-30 14:24:04 -0800

    truncate function for testing

---
+++ __tests__/test-utils/truncate.ts
@@ -0,0 +1,13 @@
+import { PrismaClient } from '@prisma/client';
+
+export async function truncate(
+  tableNames: string[],
+  prisma: PrismaClient,
+): Promise<void> {
+  const tables = tableNames.map((tableName) => `'${tableName}'`).join(', ');
+  try {
+    await prisma.$executeRawUnsafe(`TRUNCATE TABLE ${tables} CASCADE`);
+  } catch (error) {
+    console.error(error);
+  }
+}
