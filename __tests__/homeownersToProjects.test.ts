ac49301ed9c27d65b186470ac9aeb922d5958984
David <Rigby335@hotmail.com>
2023-12-18 14:34:59 -0800

    test: POST to projects with many existing homeowners fails RED

--- __tests__/homeownersToProjects.test.ts
+++
@@ -1,7 +0,0 @@
-import request from 'supertest';
-import app from '../src/app';
-
-it('#POST adds an existing homeowner to a project', async () => {
-  const res = await request(app).post('homeownertoproject');
-  expect(res.status).toBe(200);
-});
--- __tests__/projects.test.ts
+++ __tests__/projects.test.ts
@@ -162,3 +162,7 @@ it.skip('#DELETE deletes a single project by id', async () => {
   const data = await request(app).get('/projects/14');
   expect(data.body.error).toEqual('Project not found');
 });
+it('#POST creates a project with multiple homeowners', async () => {
+  const res = await request(app).post('/projects/many/["33", "34"]');
+  expect(res.status).toBe(200);
+});
