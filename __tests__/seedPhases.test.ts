b7d910ef3b00ec0c72a609fbeabd1e66db3911f4
David <Rigby335@hotmail.com>
2024-01-02 12:02:24 -0800

    test: GET seedPhases checking for server connection passes GREEN

--- __tests__/seedPhases.test.ts
+++ __tests__/seedPhases.test.ts
@@ -2,6 +2,6 @@ import request from 'supertest';
 import app from '../src/app';
 
 it('#GET gets all phases, milestones, and tasks', async () => {
-  const res = await request(app).get('/');
+  const res = await request(app).get('/seedPhases');
   expect(res.status).toBe(200);
 });
