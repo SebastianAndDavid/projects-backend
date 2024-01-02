fb6769f28d189f6f520f2e81bef1d6396a3bb1e3
David <Rigby335@hotmail.com>
2024-01-02 10:29:26 -0800

    feat: update project works

--- src/controllers/homeownersController.ts
+++ src/controllers/homeownersController.ts
@@ -33,9 +33,9 @@ export default Router()
 
       if (!id)
         return res.status(400).json({ error: 'Homeowner ID is required' });
-
+      console.log('req.body', req.body);
       const data = await HomeownersService.updateHomeownerById(id, req.body);
-
+      console.log('data', data);
       if (!data) return res.status(404).json({ error: 'Homeowner not found' });
 
       res.json(data);
