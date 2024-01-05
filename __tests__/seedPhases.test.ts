d1353458658b91c5c2961fae474cc6d5cd682616
David <Rigby335@hotmail.com>
2024-01-05 14:28:33 -0800

    test: cleaned up consoles

--- __tests__/seedPhases.test.ts
+++ __tests__/seedPhases.test.ts
@@ -1,13 +1,12 @@
 import request from 'supertest';
 import app from '../src/app';
 
-it.skip('#GET gets all phases, milestones, and tasks', async () => {
+it.skip('validating server connection', async () => {
   const res = await request(app).get('/seedPhases');
   expect(res.status).toBe(200);
 });
-it('#GET gets all phases', async () => {
+it.skip('#GET gets all phases, milestones, and tasks', async () => {
   const res = await request(app).get('/seedPhases');
   expect(res.status).toBe(200);
-  console.log('res.body', res.body[0].MilestonesReadOnly);
   expect(res.body.length).toEqual(2);
 });
